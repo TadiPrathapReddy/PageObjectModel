e20a4b460c9468f8ff0dab4b7ea066e0aa301354
Prathap Reddy Tadi <prathapreddy.jtadi@gmail.com>
2022-10-25 14:26:20 +0530

    Home page New code

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,7 +1,32 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.qa.base.TestBase;
 
 public class HomePage extends TestBase {
+	
 
+	//Pagefactory OR 
+	@FindBy(xpath = "//a[contains(text(),'Home')]")
+		WebElement HomeTab;
+		
+		
+		
+		//Intilize the  Page objects
+		public HomePage() {
+			PageFactory.initElements(driver, this);
+		}
+		//Action
+		public boolean validateHomePageTitle() {
+			System.out.println("Home page Tab is displyed");
+			return HomeTab.isDisplayed();
+			
+		}
+		
+		
+		
+		
 }
