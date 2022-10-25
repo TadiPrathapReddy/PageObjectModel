9a6e36c65bfbf81f5f61b825ef40072b9f9612bc
Prathap Reddy Tadi <prathapreddy.jtadi@gmail.com>
2022-10-25 16:08:15 +0530

    HomePageTab Code

--- src/test/java/com/qa/testcases/LoginPageTest.java
+++ src/test/java/com/qa/testcases/LoginPageTest.java
@@ -41,6 +41,7 @@ public class LoginPageTest extends TestBase {
 	@Test(priority=3)
 	public void LoginTest() throws InterruptedException {
 		homepage =login.Login();
+		homepage.validateHomePageTitle();
 	}
 	
 	@AfterMethod
